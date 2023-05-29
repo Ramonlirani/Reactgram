d192e1c077b5e6b71b4262246af98ea383d8102b
Ramon <ramonlirani@hotmail.com>
2023-05-29 14:56:54 -0300

    feat: add and delete photo

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -42,9 +42,25 @@ const loginValidation = () => {
     ]
 }
 
+const userUpdateValidation = () => {
+
+    return [
+        body("name")
+            .optional()
+            .isLength({min: 3})
+            .withMessage("O nome precisa de pelo menos 3 caracteres."),
+        body("password")
+            .optional()
+            .isLength({min: 5})
+            .withMessage("Sua senha precisa ter pelo menos 5 caracteres."),
+    ]
+
+}
+
 
 
 module.exports = {
     userCreateValidation,
-    loginValidation
+    loginValidation,
+    userUpdateValidation
 }
