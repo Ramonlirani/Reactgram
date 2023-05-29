eb1b9a4d38a566318503ce24d66c7b6e1ad4a03d
Ramon <ramonlirani@hotmail.com>
2023-05-29 20:27:51 -0300

    Finished backend/feat: searchPhotos, commentsPhoto, likePhoto, updatePhoto, getPhotoById, getAllPhoto

--- backend/middlewares/photoValidation.js
+++ backend/middlewares/photoValidation.js
@@ -20,4 +20,28 @@ const photoInsertValidation = () => {
     ]
 }
 
-module.exports = { photoInsertValidation }
+const photoUpdateValidation = () => {
+    return [
+        body("title")
+            .optional()
+            .isString()
+            .withMessage("O título é obrigatório.")
+            .isLength({min: 3})
+            .withMessage("O título precisa ter no mínimo 3 caracteres."),
+    ]
+}
+
+const commentValidation = () => {
+    return [
+        body("comment")
+            .isString()
+            .withMessage("O comentário é obrigatório.")
+    ]
+}
+
+
+module.exports = { 
+    photoInsertValidation, 
+    photoUpdateValidation,
+    commentValidation
+}
