0fef14378822372b7d2553f0230424c65ef6a9aa
Ramon <ramonlirani@hotmail.com>
2023-05-30 17:50:33 -0300

    feat: front end

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+//Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+//components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
